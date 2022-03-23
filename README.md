74d993f08d620f8f0422e2135331436c07d36bf0
Krilin39 <97433990+Krilin39@users.noreply.github.com>
2022-03-24 08:16:45 +1100

    final sub

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
-# lecture3
+Earing design tool - collaborative workflow
 
-Examples for Lecture 3
+The tool aims to help designers have a common platform to visualize the state of a design that is subject to continuous changes and different options.
 
-These examples introduce how to use RhinoCompute as a remote geometry calculation service.
+The app can be used to inform a design idea before it is released into the desired market. It uses the Rhino 3dm library to visualize accurate models and the power of option exploration through grasshopper parametric design. 
 
-- [Example 1](example1): Boolean Union with RhinoCompute
-- [Example 2](example2): Local Solving GH definitions
-- [Example 3](example3): Local Solving GH definitions with plugin
+The future aim is for the unity platform to make changes and experience the tool in real-time, for example, VR, and push the state of the design to the web through Github.
 
 
-## References
-
-- [RhinoCompute](https://developer.rhino3d.com/guides/#compute)
-- [RhinoCompute developer samples](https://github.com/mcneel/rhino-developer-samples/tree/7/compute)
-- [rhino3dm.js API reference](https://mcneel.github.io/rhino3dm/javascript/api/index.html)
-- [rhino3dm developer samples](https://github.com/mcneel/rhino-developer-samples/tree/7/rhino3dm)
-
-## Previous lectures
-
-- [Lecture 1](https://github.com/dav-leon/bimsc21-datamgmt) 
-- [Lecture 2](https://github.com/dav-leon/bimsc21-datamgmt-2)
+The earing design tool is a project of IAAC, Institute for Advanced Architecture of Catalonia developed in the Master In Advanced Computation For Architecture & Design 2021/2022 by student: Erida Bendo and Faculty: David Andres Leon, Hesham Shawqy.
---
+++ script.js
@@ -0,0 +1,294 @@
+// Import libraries
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js";
+import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@7.11.1/rhino3dm.module.js";
+import { RhinoCompute } from "https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js";
+import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js";
+
+const definitionName =  "Ear ring.gh" ;//"GH rhinocompute test 1.gh"; // "twisting-pole.gh" "rnd_node.gh" "Operating Theather test1.gh" ;
+const model = 'Sydney test1 mesh map reduced.3dm'
+
+let scene, camera, renderer, controls;
+
+// Set up sliders
+const radius_slider = document.getElementById("radius");
+radius_slider.addEventListener("mouseup", onSliderChange, false);
+radius_slider.addEventListener("touchend", onSliderChange, false);
+const count_slider = document.getElementById("count");
+count_slider.addEventListener("mouseup", onSliderChange, false);
+count_slider.addEventListener("touchend", onSliderChange, false);
+
+window.addEventListener( 'click', onClick, false);
+
+
+const loader = new Rhino3dmLoader()
+loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
+
+loader.load( model, function ( object ) {
+
+    // uncomment to hide spinner when model loads
+    // document.getElementById('loader').remove()
+    scene.add( object )
+
+} )
+
+
+loader.setLibraryPath("https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/");
+
+let rhino, definition, doc;
+rhino3dm().then(async (m) => {
+  console.log("Loaded rhino3dm.");
+  rhino = m; // global
+
+  //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
+  //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
+
+  RhinoCompute.url = "http://localhost:8081/"; //if debugging locally.
+
+  // load a grasshopper file!
+
+  const url = definitionName;
+  const res = await fetch(url);
+  const buffer = await res.arrayBuffer();
+  const arr = new Uint8Array(buffer);
+  definition = arr;
+
+  init();
+  compute();
+});
+
+async function compute() {
+  const param1 = new RhinoCompute.Grasshopper.DataTree("Radius");
+  param1.append([0], [radius_slider.valueAsNumber]);
+
+  const param2 = new RhinoCompute.Grasshopper.DataTree("Count");
+  param2.append([0], [count_slider.valueAsNumber]);
+  // clear values
+  const trees = [];
+  trees.push(param1);
+  trees.push(param2);
+  const res = await RhinoCompute.Grasshopper.evaluateDefinition(
+    definition,
+    trees
+  );
+  //console.log(res);
+  doc = new rhino.File3dm();
+  // hide spinner
+  document.getElementById("loader").style.display = "none";
+
+  //decode grasshopper objects and put them into a rhino document
+  for (let i = 0; i < res.values.length; i++) {
+    for (const [key, value] of Object.entries(res.values[i].InnerTree)) {
+      for (const d of value) {
+        const data = JSON.parse(d.data);
+        const rhinoObject = rhino.CommonObject.decode(data);
+        doc.objects().add(rhinoObject, null);
+      }
+    }
+  }
+  // go through the objects in the Rhino document
+  let objects = doc.objects();
+  for ( let i = 0; i < objects.count; i++ ) {
+  
+    const rhinoObject = objects.get( i );
+
+
+     // asign geometry userstrings to object attributes
+    if ( rhinoObject.geometry().userStringCount > 0 ) {
+      //const g_userStrings = rhinoObject.geometry().getUserStrings()
+      //rhinoObject.attributes().setUserString(g_userStrings[0][0], g_userStrings[0][1]
+      const _Area = rhinoObject.geometry().getUserStrings()[0]
+      console.log(_Area);
+    }
+  }
+  // clear objects from scene
+  scene.traverse(child => {
+    if (!child.isLight) {
+        scene.remove(child)
+    }
+})
+
+
+       
+
+
+
+  const buffer = new Uint8Array(doc.toByteArray()).buffer;
+  loader.parse(buffer, function (object) {
+
+    // go through all objects, check for userstrings and assing colors
+
+    object.traverse((child) => {
+      if (child.isLine) {
+
+        if (child.userData.attributes.geometry.userStringCount > 0) {
+          
+          //get color from userStrings
+          const colorData = child.userData.attributes.userStrings[0]
+          const col = colorData[1];
+
+          //convert color from userstring to THREE color and assign it
+          const threeColor = new THREE.Color("rgb(" + col + ")");
+          const mat = new THREE.LineBasicMaterial({ color: threeColor });
+          child.material = mat;
+        }
+      }
+    });
+
+    ///////////////////////////////////////////////////////////////////////
+    // add object graph from rhino model to three.js scene
+    scene.add(object);
+
+  });
+
+  loader.load( model, function ( object ) {
+
+    // uncomment to hide spinner when model loads
+    // document.getElementById('loader').remove()
+    scene.add( object )
+
+} )
+}
+
+function onSliderChange() {
+  // show spinner
+  document.getElementById("loader").style.display = "block";
+  compute();
+}
+
+
+// THREE BOILERPLATE //
+
+
+
+const mouse = new THREE.Vector2()
+
+
+function init() {
+  // create a scene and a camera
+  THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(1, 1, 1);
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    30000
+  );
+  camera.position.y = -60;
+
+
+  
+  // create the renderer and add it to the html
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // add some controls to orbit the camera
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // add a directional light
+  
+
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.intensity = 2;
+  scene.add(directionalLight);
+
+  const ambientLight = new THREE.AmbientLight();
+  scene.add(ambientLight);
+
+  window.addEventListener( 'click', onClick, false);
+
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  animate();
+}
+
+function meshToThreejs(mesh, material) {
+  const loader = new THREE.BufferGeometryLoader();
+  const geometry = loader.parse(mesh.toThreejsJSON());
+  return new THREE.Mesh(geometry, material);
+
+
+}
+
+function onClick( event ) {
+
+    console.log( `click! (${event.clientX}, ${event.clientY})`)
+
+	// calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1
+    
+    raycaster.setFromCamera( mouse, camera )
+
+	// calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children, true )
+
+    let container = document.getElementById( 'container' )
+    if (container) container.remove()
+
+    // reset object colours
+    scene.traverse((child, i) => {
+        if (child.isMesh) {
+            child.material.color.set( 'white' )
+        }
+    });
+
+    if (intersects.length > 0) {
+
+        // get closest object
+        const object = intersects[0].object
+        console.log(object) // debug
+
+        object.material.color.set( 'yellow' )
+
+        // get user strings
+        let data, count
+        if (object.userData.attributes !== undefined) {
+            data = object.userData.attributes.userStrings
+        } else {
+            // breps store user strings differently...
+            data = object.parent.userData.attributes.userStrings
+        }
+
+        // do nothing if no user strings
+        if ( data === undefined ) return
+
+        console.log( data )
+        
+        // create container div with table inside
+        container = document.createElement( 'div' )
+        container.id = 'container'
+        
+        const table = document.createElement( 'table' )
+        container.appendChild( table )
+
+        for ( let i = 0; i < data.length; i ++ ) {
+
+            const row = document.createElement( 'tr' )
+            row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
+            table.appendChild( row )
+        }
+
+        document.body.appendChild( container )
+    }
+     
+
+}
+
+
+
